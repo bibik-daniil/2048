67f6e17a04a7e295ab66fcbc82bf8b28b781ecf8
Bibik Daniil <daniil.bibik@mail.ru>
2026-06-15 23:21:52 +0300

    Создание звука

--- src/components/GameContainer/GameContainer.tsx
+++ src/components/GameContainer/GameContainer.tsx
@@ -11,17 +11,55 @@ import { checkGameOver } from "../../script/checkGameOver";
 import { startNewGame } from "../../script/newGame";
 import { setContainer } from "../../redux/containerSlice";
 import { setGameOver } from "../../redux/gameOverSlice";
+import useSound from "use-sound";
+import ten from "../../sound/ten.mp3"
+import twenty from "../../sound/twenty.wav"
+import fourty from "../../sound/fourty.wav"
+import oneHundred from "../../sound/oneHundred.wav"
+import thousand from "../../sound/thousand.mp3"
+import lose from "../../sound/gameOver.mp3"
 
 const GameContainer = () => {
+  const score = useSelector((state: RootState) => state.score);
   const square = useSelector((state: RootState) => state.square);
   const container = useSelector((state: RootState) => state.container);
   const gameOver = useSelector((state: RootState) => state.gameOver);
 
+  const [tenSound] = useSound(ten, {volume: 1})
+  const [twentySound] = useSound(twenty, {volume: 1})
+  const [fourtySound] = useSound(fourty, {volume: 1})
+  const [oneHundredSound] = useSound(oneHundred, {volume: 1})
+  const [thousandSound] = useSound(thousand, {volume: 1})
+  const [gameOverSound] = useSound(lose, {volume: 1})
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(setContainer(startNewGame(square, dispatch)));
   }, [square]);
+
+  useEffect(() => {
+    if (gameOver) {
+      gameOverSound();
+    }
+  }, [gameOver, gameOverSound])
+
+  useEffect(() => {
+    if (score.increase > 0 && score.increase <= 10) {
+      tenSound();
+    }
+    if (score.increase > 10 && score.increase <= 20) {
+      twentySound();
+    }
+    if (score.increase > 20 && score.increase <= 40) {
+      fourtySound();
+    }
+    if (score.increase > 40 && score.increase <= 100) {
+      oneHundredSound();
+    }
+    if (score.increase > 100) {
+      thousandSound();
+    }
+  }, [score, tenSound])
 
   useEffect(() => {
     const keystroke = (event: KeyboardEvent) => {
--- src/components/Menu/NewGame.tsx
+++ src/components/Menu/NewGame.tsx
@@ -3,17 +3,26 @@ import { useDispatch, useSelector } from "react-redux";
 import { setContainer } from "../../redux/containerSlice";
 import { RootState } from "../../redux/store";
 import { startNewGame } from "../../script/newGame";
+import useSound from "use-sound";
+import start from "../../sound/start.mp3"
 
 const NewGame = () => {
+  const [startSound] = useSound(start, {volume: 1})
   const square = useSelector((state: RootState) => state.square);
   
   const dispatch = useDispatch()
 
+  const newGame = () => {
+    startSound();
+    dispatch(setContainer(startNewGame(square, dispatch)));
+  };
+
   return (
-    <>
-      <button onClick={() => dispatch(setContainer(startNewGame(square, dispatch)))}>NewGame</button>
-    </>
+    <button onClick={newGame}>
+      New Game
+    </button>
   );
 };
+
 
 export default NewGame;
